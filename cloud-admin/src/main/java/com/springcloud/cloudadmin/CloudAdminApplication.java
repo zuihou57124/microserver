b40b7983ee4d129d5762871a35663b78af57f086
zuihou57124 <1545409483@qq.com>
2020-04-10 23:20:31 +0800

    springcloud-demo

---
+++ cloud-admin/src/main/java/com/springcloud/cloudadmin/CloudAdminApplication.java
@@ -0,0 +1,17 @@
+package com.springcloud.cloudadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+public class CloudAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAdminApplication.class, args);
+    }
+
+}
